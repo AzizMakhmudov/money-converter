360ebd478ece40caa2ce9dfaed010ac03f4f828d
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2024-03-04 19:07:04 +0500

    add new title

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-let elForm =  document.querySelector("#form");
+let elForm = document.querySelector("#form");
 let elInput = document.querySelector(".input-text");
-let elUSD =   document.querySelector("#dollar");
-let elRUBL =  document.querySelector("#rub");
-let elEURO =  document.querySelector("#euro");
+let elUSD = document.querySelector("#dollar");
+let elRUBL = document.querySelector("#rub");
+let elEURO = document.querySelector("#euro");
 let elHeading = document.querySelector(".h1-value");
 let elSelect = document.querySelector("#elSelect");
-let kursD = 0.000080;
+let kursD = 0.00008;
 let kursE = 0.000074;
 let kursR = 0.0073;
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   if (elSelect.value === "USD") {
-    elHeading.textContent = `USD ${(elInput.value * kursD.toFixed(6))}`;
+    elHeading.textContent = `USD ${elInput.value * kursD.toFixed(6)}`;
   } else if (elSelect.value === "RUB") {
-    elHeading.textContent = `RUB ${(elInput.value * kursR.toFixed(6))}`;
+    elHeading.textContent = `RUB ${elInput.value * kursR.toFixed(6)}`;
   } else if (elSelect.value === "EURO") {
-    elHeading.textContent = `EURO ${(elInput.value * kursE.toFixed(6))}`;
+    elHeading.textContent = `EURO ${elInput.value * kursE.toFixed(6)}`;
   }
 });
