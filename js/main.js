60f69a205e41dc2bf8464a2026b869d00f4fdf9f
AzizMakhmudov <azizmakhmudov2412@gmail.com>
2024-03-04 19:04:10 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -5,16 +5,16 @@ var elRUBL =  document.querySelector("#rub");
 var elEURO =  document.querySelector("#euro");
 var elHeading = document.querySelector(".h1-value");
 var elSelect = document.querySelector("#elSelect");
-var kursD = 11.140;
-var kursE = 10.813;
-var kursR = 177.81;
+var kursD = 0.000080;
+var kursE = 0.000074;
+var kursR = 0.0073;
 elForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   if (elSelect.value === "USD") {
-    elHeading.textContent = `USD ${(elInput.value / kursD).toFixed(2)}`;
+    elHeading.textContent = `USD ${(elInput.value * kursD.toFixed(6))}`;
   } else if (elSelect.value === "RUB") {
-    elHeading.textContent = `RUB ${(elInput.value / kursR).toFixed(2)}`;
+    elHeading.textContent = `RUB ${(elInput.value * kursR.toFixed(6))}`;
   } else if (elSelect.value === "EURO") {
-    elHeading.textContent = `EURO ${(elInput.value / kursE).toFixed(2)}`;
+    elHeading.textContent = `EURO ${(elInput.value * kursE.toFixed(6))}`;
   }
 });
